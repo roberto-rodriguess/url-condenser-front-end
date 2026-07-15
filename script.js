10278fe1358cf399260e633f13794127df18724c
Rob <robertorodriguesre@gmail.com>
2026-07-14 21:53:35 -0300

    melhorando a identação

--- script.js
+++ script.js
@@ -1,229 +1,108 @@
 const API = "http://localhost:8080";
 
-
-
-async function createUrl(){
-
-
+async function createUrl() {
     const input = document.getElementById("urlInput");
-
-
     const url = input.value;
 
-
-
-    if(!url){
-
+    if (!url) {
         alert("Digite uma URL");
-
         return;
-
     }
-
-
 
     const response = await fetch(
         `${API}/api/urls`,
         {
-
-            method:"POST",
-
-            headers:{
-
-                "Content-Type":"application/json"
-
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
             },
 
-            body:JSON.stringify({
-
-                originalUrl:url
-
+            body: JSON.stringify({
+                originalUrl: url
             })
-
         }
-
     );
 
-
-
     const data = await response.json();
-
-
-
     document.getElementById("result").innerHTML = `
-
         <p>
         Sua URL curta:
         </p>
-
         <a href="${data.shortUrl}" target="_blank">
         ${data.shortUrl}
         </a>
-
-
         <button class="copy"
         onclick="copyUrl('${data.shortUrl}')">
         Copiar
-        </button>
+        </button>`;
 
-    `;
-
-
-    input.value="";
-
-
+    input.value = "";
     loadUrls();
-
-
 }
 
-
-
-
-async function loadUrls(){
-
-
+async function loadUrls() {
     const response = await fetch(
         `${API}/api/urls`
     );
 
-
     const urls = await response.json();
-
-
-
     const table =
-    document.getElementById("urlTable");
+        document.getElementById("urlTable");
 
-
-
-    table.innerHTML="";
-
-
+    table.innerHTML = "";
 
     urls.forEach(url => {
-
-
-
         table.innerHTML += `
-
-
         <tr>
-
-
         <td>
-
         <a href="${url.originalUrl}" target="_blank">
-
         ${url.originalUrl}
-
         </a>
-
         </td>
-
-
-
         <td>
-
         <a href="${API}/${url.shortCode}" target="_blank">
-
         ${url.shortCode}
-
         </a>
 
-
         </td>
-
-
-
         <td>
-
         ${url.clicks}
-
         </td>
 
-
-
         <td>
-
-
         <button 
         class="copy"
         onclick="copyUrl('${API}/${url.shortCode}')">
-
         Copiar
-
         </button>
-
-
         <button
         class="delete"
         onclick="deleteUrl(${url.id})">
-
         Excluir
-
         </button>
 
-
-
         </td>
-
-
         </tr>
-
-
         `;
-
-
-
     });
-
-
-
 }
 
-
-
-
-async function deleteUrl(id){
-
-
+async function deleteUrl(id) {
     await fetch(
-
         `${API}/api/urls/${id}`,
-
         {
-
-            method:"DELETE"
-
+            method: "DELETE"
         }
-
     );
 
-
     loadUrls();
-
-
 }
 
-
-
-
-function copyUrl(url){
-
-
+function copyUrl(url) {
     navigator.clipboard.writeText(url);
-
-
     alert(
         "URL copiada!"
     );
-
-
 }
-
-
-
-
 
 loadUrls();
